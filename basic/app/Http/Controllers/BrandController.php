afc03e2a332ca3a195b4db0fae2c63837cdbcb8c
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-11-06 17:13:54 +0900

    Lesson46: Middleware Auth
    Lesson46: Middleware Auth

--- basic/app/Http/Controllers/BrandController.php
+++ basic/app/Http/Controllers/BrandController.php
@@ -10,6 +10,11 @@ use Image;
 
 class BrandController extends Controller
 {
+    public function __construct()  
+    {
+        $this->middleware('auth');
+    }
+    
     public function AllBrand()
     {
         $brands = Brand::latest()->paginate(5);
--- basic/app/Http/Controllers/ContactController.php
+++ basic/app/Http/Controllers/ContactController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    public function __construct()  
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(){
         //echo "This is our Contact Page";
         return view('contact');
