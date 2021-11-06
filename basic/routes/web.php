839c4f3483d8fab9360c18f712c16c3acab65309
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-11-06 16:21:11 +0900

    Lesson45: Email Verify
    Lesson45: Email Verify

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
 Route::get('/', function () {
     return view('welcome');
 });
